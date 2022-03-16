299058a172199ceca012025cbff800628f205474
Jayesh Gohel <jayesh.gohel@gmail.com>
2022-03-16 15:42:55 +0530

    Adding home page placeholder text

--- src/views/CategoryPage.vue
+++ src/views/CategoryPage.vue
@@ -1,26 +1,16 @@
 <template>
   <div class="container main">
-    <h2 class="page-title">{{ category }}</h2>
-    <!-- 
-    <div :class="classObject" class="row gx-5 gy-3 mb-5 masonry mx-5">
-      <template v-for="story in filtered_stories" :key="story.ID">
-        <div v-if="story.image">
-          <img
-            :src="story.image.thumb"
-            alt=""
-            class="img-fluid w-100 shadow border"
-          />
-          <h5 class="mt-3">{{ story.TITLE }}</h5>
-          <p>
-            {{ story.AUTHORS }} <span v-if="story.AUTHORS">,</span>
-            {{
-              story.COUNTRY.charAt(0).toUpperCase() +
-              story.COUNTRY.toLowerCase().slice(1)
-            }}
-          </p>
-        </div>
-      </template>
-    </div> -->
+    <div class="row mb-5">
+      <div class="col-2 align-right">
+        <div
+          class="ratio ratio-1x1 bg-light rounded-circle float-end"
+          style="width: 80px"
+        ></div>
+      </div>
+      <div class="col align-items-center d-flex">
+        <h2 class="page-title">{{ category }}</h2>
+      </div>
+    </div>
 
     <div
       v-masonry
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,66 @@
 <template>
-  <div>Home page content</div>
+  <div class="row justify-content-center">
+    <div class="col-md-4">
+      <h4>Curatorial note</h4>
+      <p>
+        A new "culture of water" cannot grow without memory. Memories, whether
+        they are sensory or emotional, short-term or long-term, material or
+        immaterial, are fundamental to our existence as individuals and as
+        collective societies.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+        laudantium molestiae obcaecati vitae dolorum in nobis consectetur
+        nostrum omnis magnam maiores officiis, eveniet repellat adipisci saepe,
+        praesentium illo sunt facilis!
+      </p>
+    </div>
+    <div class="col-md-4">
+      <p>
+        Asperiores at cumque vero aut, fugit beatae, consectetur vel aperiam
+        dolor ex nostrum doloremque odit adipisci amet sapiente assumenda nulla
+        et mollitia eligendi temporibus quaerat! Porro velit voluptatem fugit
+        blanditiis?
+      </p>
+      <p>
+        Nulla quas, nam voluptas harum explicabo illo id illum rem esse est
+        blanditiis? Ut sunt cupiditate sequi enim harum suscipit asperiores
+        atque, eum mollitia obcaecati fugit animi quo officia consequatur!
+      </p>
+    </div>
+  </div>
+
+  <hr />
+
+  <div class="row">
+    <div class="col-md-6 mx-auto text-center py-3">
+      <div
+        class="d-inline-block rounded-circle bg-light ratio ratio-1x1"
+        style="width: 80px"
+      ></div>
+      <h3 class="text-center my-3 text-uppercase">Exhibition</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis,
+        laboriosam sunt, dignissimos similique quam sed voluptatum sequi
+        exercitationem distinctio ut voluptatibus? Doloribus commodi praesentium
+        cumque debitis. Consectetur eaque enim sed?
+      </p>
+      <a href="/category/Exhibition" class="d-inline-block btn btn-primary my-3"
+        >VIEW</a
+      >
+    </div>
+  </div>
+
+  <hr />
+
+  <div class="row">
+    <div class="col-md-6 mx-auto text-center py-3">
+      <h3 class="text-center my-3 text-uppercase">Categories</h3>
+      <p>Grid of categories here.</p>
+      <a href="#" class="d-inline-block btn btn-primary my-3">VIEW</a>
+    </div>
+  </div>
+  <hr class="mb-5" />
 </template>
 
 <script>
