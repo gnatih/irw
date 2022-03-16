7b6fe07678b97cf494f1980f7dffe76bd9d52c2b
Jayesh Gohel <jayesh.gohel@gmail.com>
2022-03-16 14:48:58 +0530

    Story page layout added

--- src/views/StoryPage.vue
+++ src/views/StoryPage.vue
@@ -1,23 +1,42 @@
 <template>
   <div class="col-md-8 mx-auto mb-5">
-    <swiper
-      v-if="stories.length > 1"
-      id="story-swiper"
-      :modules="modules"
-      :pagination="{ clickable: true }"
-      navigation
-      @slideChange="slideChange"
-    >
-      <template v-for="story in stories">
-        <swiper-slide
-          v-if="story.image"
-          :key="story.ID"
-          class="story-slide"
-          :style="{ background: `url(${story.image.large})` }"
-        >
-        </swiper-slide>
-      </template>
-    </swiper>
+    <div v-if="stories.length > 1">
+      <swiper
+        id="story-swiper"
+        :modules="[Navigation, Thumbs]"
+        :pagination="{ clickable: true }"
+        navigation
+        @slide-change="slideChange"
+        :thumbs="{ swiper: thumbsSwiper }"
+      >
+        <template v-for="story in stories" :key="story.ID">
+          <swiper-slide
+            v-show="story.image"
+            class="story-slide"
+            :style="{ background: `url(${story.image.large})` }"
+          >
+          </swiper-slide>
+        </template>
+      </swiper>
+      <swiper
+        :modules="[Thumbs]"
+        watch-slides-progress
+        class="mt-3 thumbs-swiper"
+        slides-per-view="5"
+        :center-slides="true"
+        :center-insufficient-slides="true"
+        :space-between="15"
+        @swiper="setThumbsSwiper"
+      >
+        <template v-for="story in stories" :key="story.ID">
+          <swiper-slide
+            v-show="story.image"
+            :style="{ 'background-image': `url(${story.image.thumb})` }"
+          >
+          </swiper-slide>
+        </template>
+      </swiper>
+    </div>
     <div
       v-else
       class="story-slide"
@@ -51,11 +70,10 @@
 import { computed, inject, ref } from "@vue/runtime-core";
 import { useRoute } from "vue-router";
 import { Swiper, SwiperSlide } from "swiper/vue";
-import { Pagination, Navigation } from "swiper";
+import { Navigation, Thumbs } from "swiper";
 
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/pagination";
 
 export default {
   components: { Swiper, SwiperSlide },
@@ -63,6 +81,7 @@ export default {
     const store = inject("store");
     const route = useRoute();
     const slide = ref(0);
+    const thumbsSwiper = ref(null);
     const sid = route.params.sid;
     const stories = computed(() => {
       return store.state.data.filter((story) => parseInt(story.ID) == sid);
@@ -72,12 +91,19 @@ export default {
       slide.value = i.activeIndex;
     };
 
+    const setThumbsSwiper = (swiper) => {
+      thumbsSwiper.value = swiper;
+    };
+
     return {
       store,
       stories,
-      modules: [Pagination, Navigation],
+      Navigation,
+      Thumbs,
       slide,
       slideChange,
+      thumbsSwiper,
+      setThumbsSwiper,
     };
   },
 };
