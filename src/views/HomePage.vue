481312160f620a83faac913c2aa003a8dc87a5d3
Jayesh Gohel <jayesh.gohel@gmail.com>
2022-05-07 18:44:02 +0530

    Working on homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,62 +1,105 @@
 <template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <img
+          src="/images/fp-sequence1.jpg"
+          alt="I Remember Water.."
+          class="img-fluid"
+        />
+      </div>
+    </div>
+  </div>
+
   <div class="row justify-content-center">
-    <div class="col-md-4">
-      <h4>Curatorial note</h4>
+    <div class="col-md-8">
+      <div class="curatorial-note text-uppercase text-primary fw-bold my-3">
+        Curatorial note
+      </div>
+    </div>
+    <div class="col-md-8 text-columns-2">
       <p>
-        A new "culture of water" cannot grow without memory. Memories, whether
+        A new “culture of water” cannot grow without memory. Memories, whether
         they are sensory or emotional, short-term or long-term, material or
         immaterial, are fundamental to our existence as individuals and as
-        collective societies.
+        collective societies. The curatorial team of this exhibition selected a
+        number of meaningful images that recall our past water memories and
+        reflect on pathways that can help us shape our water futures.
       </p>
+
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
-        laudantium molestiae obcaecati vitae dolorum in nobis consectetur
-        nostrum omnis magnam maiores officiis, eveniet repellat adipisci saepe,
-        praesentium illo sunt facilis!
+        From the ornate public fountains and standpipes that provided free water
+        to citizens in bustling cities, to the ancient civilizations that have
+        flourished along rivers and waterways, water has shaped our landscape
+        across time and space, from Europe to Africa, from Asia to the Americas.
       </p>
-    </div>
-    <div class="col-md-4">
+
       <p>
-        Asperiores at cumque vero aut, fugit beatae, consectetur vel aperiam
-        dolor ex nostrum doloremque odit adipisci amet sapiente assumenda nulla
-        et mollitia eligendi temporibus quaerat! Porro velit voluptatem fugit
-        blanditiis?
+        However, while water is at the core of our bodies, our livelihoods and
+        our social relationships, water resources have been dammed, depleted or
+        polluted by human activity engrained in engineering paradigms, economic
+        development and growing transboundary conflicts. It's likely that in the
+        future water will have enormous economic value, as the 'new petroleum'.
       </p>
+
       <p>
-        Nulla quas, nam voluptas harum explicabo illo id illum rem esse est
-        blanditiis? Ut sunt cupiditate sequi enim harum suscipit asperiores
-        atque, eum mollitia obcaecati fugit animi quo officia consequatur!
+        Climate change has not only accentuated our failure to use and manage
+        water wisely, but the dangers of too little or too much water (drought,
+        floods and rising sea levels) have posed new challenges to global water
+        governance.
+      </p>
+
+      <p>
+        If we want to address the next global water challenges, we need to find
+        new ways to connect people with their water heritage, so that we can
+        build empathy and stimulate new water awareness, supporting the wise and
+        judicious use of our water legacies. In the future, water museums have a
+        pivotal role to play in this process.
       </p>
     </div>
   </div>
 
-  <hr />
+  <div class="break--top"></div>
 
   <div class="row">
-    <div class="col-md-6 mx-auto text-center py-3">
+    <div class="col-md-6 mx-auto py-3">
       <div
         class="d-inline-block rounded-circle bg-light ratio ratio-1x1"
         style="width: 80px"
       ></div>
-      <h3 class="text-center my-3 text-uppercase">Exhibition</h3>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis,
-        laboriosam sunt, dignissimos similique quam sed voluptatum sequi
-        exercitationem distinctio ut voluptatibus? Doloribus commodi praesentium
-        cumque debitis. Consectetur eaque enim sed?
-      </p>
-      <a href="/category/Exhibition" class="d-inline-block btn btn-primary my-3"
-        >VIEW</a
-      >
+      <h3 class="my-3 text-uppercase">Exhibition</h3>
+      <div class="bg-light p-4 d-flex">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis,
+          laboriosam sunt, dignissimos similique quam sed voluptatum sequi
+          exercitationem distinctio ut voluptatibus? Doloribus commodi
+          praesentium cumque debitis. Consectetur eaque enim sed?
+        </p>
+        <div class="mx-4">
+          <a
+            href="/category/Exhibition"
+            class="d-inline-block btn btn-secondary my-3 text-uppercase"
+            >VIEW</a
+          >
+        </div>
+      </div>
     </div>
   </div>
-
-  <hr />
 
   <div class="row">
     <div class="col-md-6 mx-auto text-center py-3">
       <h3 class="text-center my-3 text-uppercase">Categories</h3>
-      <p>Grid of categories here.</p>
+      <p>
+        In all, we received around 600 images and the curatorial team selected
+        462 from these to show vibrant and historical relationships with the
+        most fundamental element of life. From water as the elixir of Life to
+        recreation, Play and education; from water Heritage to our Spiritual and
+        sacred connections with water; from water shaping our proximity and
+        Belonging to a river, or a place, to the radical Transformations of
+        waterscapes witnessed in the last century or so - each category was
+        selected by an individual curator after hours of inspiring collective
+        discussions.
+      </p>
       <a href="#" class="d-inline-block btn btn-primary my-3">VIEW</a>
     </div>
   </div>
