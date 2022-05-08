b516695eb77218d233a8e15266b4992c9e478151
Jayesh Gohel <jayesh.gohel@gmail.com>
2022-05-08 23:28:11 +0530

    --ament

---
+++ src/components/CategoryHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="col d-flex align-items-center">
+    <div class="ratio ratio-1x1 float-end" style="width: 100px">
+      <img :src="`/images/icon_${category.toLowerCase()}.svg`" alt="" />
+    </div>
+    <h2 class="page-title me-auto">{{ category }}</h2>
+
+    <img :src="`/images/il_${category.toLowerCase()}.svg`" width="200" :alt="category" class="ms-auto" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: { category: String },
+};
+</script>
